b0bf23d5a41fe4b072829f84c0315bb83cedcd19
qaa <1234@qaa.com>
2020-08-25 15:46:52 +0800

    FFmpeg内存模型测试代码

---
+++ avframe.h
@@ -0,0 +1,7 @@
+﻿#ifndef AVFRAME_H
+#define AVFRAME_H
+
+#include "libavcodec/avcodec.h"
+
+void av_frame_test();
+#endif // AVFRAME_H
---
+++ main.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+#include "avpacket.h"
+#include "avframe.h"
+int main()
+{
+    printf("Hello World!\n");
+  //  av_packet_test();
+    av_frame_test();
+    return 0;
+}
